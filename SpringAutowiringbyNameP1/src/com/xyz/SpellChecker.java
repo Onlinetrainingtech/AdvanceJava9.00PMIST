6bd5f471a92d430de079642f674c9ff001890df0
MohamedAzarudeen <azartechnet@gmail.com>
2023-09-10 22:58:08 +0530

    updated

---
+++ SpringAutowiringbyNameP1/src/com/xyz/SpellChecker.java
@@ -0,0 +1,14 @@
+package com.xyz;
+
+public class SpellChecker 
+{
+   public SpellChecker()
+   {
+	   System.out.println("Inside the SpellChecker");
+	   
+   }
+   public void checkSpelling()
+   {
+	   System.out.println("Inside the checkSpelling..");
+   }
+}
